1c98aa3abfdb42dcf94085f1f2eed57e482df874
Anurag Dadheech <anuragdadheech1990@gmail.com>
2014-12-19 20:16:40 +0530

    Not working right now, have to fix

---
+++ poker.js
@@ -0,0 +1,18 @@
+
+window.setInterval(function(){
+	poke();
+}, 5000);
+function poke(){
+	pokeList(document.getElementsByClassName("_42ft"));
+}
+
+function pokeList(list){
+	for(var i=0; i<list.length; i++) {
+		var e = list[i];
+		if ( (e.getAttribute('rel') == 'async-post') && (e.getAttribute("ajaxify").substring(0,13) == "/pokes/inline") && !(e.getAttribute("ajaxify").indexOf("suggestion") > -1) && !(e.getAttribute("ajaxify").indexOf("is_hide=1") > -1) ) {
+				click = document.createEvent('MouseEvents');
+				click.initEvent( 'click', true, true );
+				e.dispatchEvent(click);
+		}
+	}
+}
